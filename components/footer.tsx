4d78a1ed1c450584660efddf4aaab00c1501dac4
Wake7401 <91364482+Wake7401@users.noreply.github.com>
2024-05-12 20:53:24 +0700

    create navbar

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "@/components/ui/container";
+
+const Footer = () => {
+  return (
+    <div className="bg-black w-full h-[60px] flex items-center">
+      <Container >
+        <span className="text-white">Copyright © 2024. All Rights Reserved By AnimeList</span>
+      </Container>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ type.ts
@@ -0,0 +1,6 @@
+export interface Anime {
+    mal_id: string;
+    image_url: string;
+    title: string,
+    title_english: string;
+}
