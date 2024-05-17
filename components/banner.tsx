1c3b4234bd5197eee25f723f258a523621ba7ab4
Wake7401 <91364482+Wake7401@users.noreply.github.com>
2024-05-17 20:34:10 +0700

    create banner

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <Swiper
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      modules={[Autoplay]}
+      className="w-full h-full mt-5"
+    >
+      <SwiperSlide>
+        <img
+          src="https://s4.anilist.co/file/anilistcdn/media/anime/banner/163270-QshLCttd04s6.jpg"
+          alt=""
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://s4.anilist.co/file/anilistcdn/media/anime/banner/153518-7uRvV7SLqmHV.jpg"
+          alt=""
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://s4.anilist.co/file/anilistcdn/media/anime/banner/163270-QshLCttd04s6.jpg"
+          alt=""
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src="https://s4.anilist.co/file/anilistcdn/media/anime/banner/153518-7uRvV7SLqmHV.jpg"
+          alt=""
+        />
+      </SwiperSlide>
+    </Swiper>
+  );
+};
 
-export default Banner
+export default Banner;
